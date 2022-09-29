89f0eae4b422079e9f250b85141378178366249b
faridrma <faridrma@code.edu.az>
2022-09-29 15:59:34 +0400

    commit

---
+++ ProjectNumber_1/NewFolder/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectNumber_1.NewFolder
+{
+    public class Department
+    {
+        public string Name;
+        public double WorkerLimit;
+        public double SalaryLimit;
+        public string Employees;
+
+        public Employee[] Employees;
+        public Department()
+        {
+            Employee = new Employee[0];
+        }
+
+        internal Employee[] Employee { get; }
+
+        public double CalcSalaryAverage()
+        {
+             
+        }
+
+    }
+}
---
+++ ProjectNumber_1/NewFolder/IHumanResourceManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectNumber_1.NewFolder
+{
+    public interface IHumanResourceManager
+    {
+        Department[] Departments { get; }
+        public void AddDepartment();
+        Department[] GetDepartments();
+        Department[] EditDepartments();
+        public void AddEmployee();
+        public void RemoveEmployee();
+        public void EditEmploye();
+
+        
+
+
+    }
+}
